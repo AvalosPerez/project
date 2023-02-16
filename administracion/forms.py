ae8f3462be01550cee7a28ad805c9713e671836e
nfuentesp <javalosp@unemi.edu.ec>
2023-02-16 10:46:45 -0500

    diseño de los formularios

--- administracion/forms.py
+++ administracion/forms.py
@@ -15,15 +15,67 @@ class ModuloForm(forms.ModelForm):
         fields = '__all__'
         exclude=("status",)
 
+        widgets = {
+            'nombre': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'descripcion': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+
+             'name_tag': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+
+             'activo': forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input',
+                    'col': 'col-md-12',
+                    'imputstyle': 'form-check form-switch toggle-switch',
+                    'type':'checkbox',
+
+                }
+            ),
+
+        }
+
 
 class EmpresaForm(forms.ModelForm):
 
     def __init__(self, *args,**kwargs):
         super(EmpresaForm, self).__init__(*args, **kwargs)
 
+
     class Meta:
         model = Empresa
         fields = '__all__'
         exclude=("status",)
+
+        widgets = {
+            'nombre': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+
+        }
 
 
--- inventario/forms.py
+++ inventario/forms.py
@@ -4,48 +4,242 @@ from inventario.models import Categoria, UnidadMedida, Insumo, Proveedor
 
 class CategoriaForm(forms.ModelForm):
 
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super(CategoriaForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = ' form-control   form-row '
             visible.field.widget.attrs['fieldset_class'] = ' input-style-1'
+
     class Meta:
         model = Categoria
         fields = '__all__'
-        exclude=("status",)
+        exclude = ("status",)
+
+        widgets = {
+            'descripcion': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+
+        }
 
 
 class UnidadMedidaForm(forms.ModelForm):
 
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super(UnidadMedidaForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = ' form-control   form-row '
-            visible.field.widget.attrs['fieldset_class'] = ' input-style-1'
+
     class Meta:
         model = UnidadMedida
         fields = '__all__'
-        exclude=("status",)
+        exclude = ("status",)
+
+        widgets = {
+            'descripcion': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'alias': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+        }
 
 
 class InsumoForm(forms.ModelForm):
 
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super(InsumoForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = Insumo
         fields = '__all__'
-        exclude=("status",)
+        exclude = ("status",)
+
+        widgets = {
+            'categoria': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'select-style-1'
+
+                }
+            ),
+            'unidad_medida': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'select-style-1'
+
+                }
+            ),
+            'lote': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'descripcion': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'detalle': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'presentacion_comercial': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-6',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+            'fecha_vencimiento': forms.DateInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-6',
+                    'imputstyle': 'input-style-1',
+                    'type':'date'
+
+                }
+            ),
+
+             'minimo': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-6',
+                    'imputstyle': 'input-style-1',
+                    'type':'number',
+                    'min':'0'
+
+                }
+            ),
+
+
+             'maximo': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-6',
+                    'imputstyle': 'input-style-1',
+                    'type':'number',
+                    'min':'0'
+
+                }
+            ),
+
+
+            'proveedor': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'select-style-1'
+
+                }
+            ),
+
+
+             'cantidad': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'input-style-1',
+                    'type':'number',
+                    'min':'0'
+
+                }
+            ),
+
+
+
+             'costo_unitario': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'input-style-1',
+                    'type':'number',
+                    'min':'0'
+
+                }
+            ),
+
+
+             'precio_venta': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-4',
+                    'imputstyle': 'input-style-1',
+                    'type':'number',
+                    'min':'0',
+                    'decimal': '2'
+
+                }
+            ),
+
+        }
+
 
 class ProveedorForm(forms.ModelForm):
 
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super(ProveedorForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = ' form-control   form-row '
-            visible.field.widget.attrs['fieldset_class'] = ' input-style-1'
+
     class Meta:
         model = Proveedor
         fields = '__all__'
-        exclude=("status",)
+        exclude = ("status",)
+
+        widgets = {
+            'razon_social': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1'
+
+                }
+            ),
+
+            'email': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1',
+                    'placeholder': 'example@gmail.com'
+                }
+            ),
+
+            'telefono': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'col': 'col-md-12',
+                    'imputstyle': 'input-style-1',
+                    'type': 'tel',
+                    'maxlength': '10',
+                    'placeholder': '099-9999-9999'
+
+                }
+            ),
+
+        }

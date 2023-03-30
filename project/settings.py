52a8ec6e91d53e7138fbf08aa06a62051deb0dfe
nfuentesp <nfuentesp@unemi.edu.ec>
2023-03-30 16:41:37 -0500

    .

--- project/settings.py
+++ project/settings.py
@@ -142,7 +142,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'systeminv345@gmail.com'
-EMAIL_HOST_PASSWORD = 'yoinerxpaupoklok'
+EMAIL_HOST_PASSWORD = 'hftmwqzcrnsoemkt'
 EMAIL_PORT = 587
 
 #
